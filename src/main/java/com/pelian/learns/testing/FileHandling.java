fedcb3b0d58a905273af7714aee393038ca27468
Incomprehensive <noaccount51009@outlook.com>
2017-07-10 13:45:17 +0300

    Cleaning and tidying up the code. Login and WebElement moved to another class

--- src/main/java/com/pelian/learns/testing/FileHandling.java
+++ src/main/java/com/pelian/learns/testing/FileHandling.java
@@ -1,5 +1,12 @@
 package com.pelian.learns.testing;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -12,12 +19,50 @@ import java.util.Scanner;
  * Created by Anton Pelianski on 08.07.2017.
  */
 public class FileHandling {
+    @FindBy(xpath = "//input[@name = 'login_username']")
+    protected WebElement username;
+
+    @FindBy (xpath = "//input[@name = 'login_password']")
+    protected WebElement pass;
+
+    @FindBy (xpath = "//div[@class = 'logged-in-as-wrap']/a[2]")
+    protected WebElement logoutButton;
+
+    @FindBy (xpath = "//span[@class = 'a-like bold']")
+    protected WebElement loginButtonForm;
+
+    @FindBy (xpath = "//input[@name = 'login']")
+    protected WebElement loginButtonSubmit;
+
+    @FindBy (xpath = "//div[@class = 'logged-in-as-wrap']/a[1]")
+    protected WebElement loggedUser;
+
+    @FindBy (xpath = "//input[@id = 'search-text']")
+    protected WebElement searchInput;
+
+    @FindBy (xpath = "//input[@id = 'search-submit']")
+    protected WebElement searchSubmit;
+
+    protected WebDriver driver;
+
+    protected String website = "https://rutracker.org";
+
     public String user;
+
     public String password;
+
+    protected WebDriverWait hold;
+
     File file = new File("src/main/resources/login.txt");
+
     File logFile;
 
+    /**
+     * Method to read credentials from file
+     * @throws FileNotFoundException
+     */
     public void read() throws FileNotFoundException {
+        // Read a file with credentials to ger username and password for login method
         Scanner br = new Scanner(file).useDelimiter(";");
         if (br.hasNext()) {
             user = br.next();
@@ -28,9 +73,14 @@ public class FileHandling {
         br.close();
     }
 
+    /**
+     * Method to log our actions into a log file
+     * @param log
+     * @throws IOException
+     */
     public void logToFile(String log) throws IOException {
-        String today = new SimpleDateFormat("yyyy_MM_dd").format(Calendar.getInstance().getTime());;
-        String logTime = new SimpleDateFormat("yyyy_MM_dd_HH:mm:ss").format(Calendar.getInstance().getTime());;
+        String today = new SimpleDateFormat("yyyy_MM_dd").format(Calendar.getInstance().getTime());
+        String logTime = new SimpleDateFormat("yyyy_MM_dd_HH:mm:ss").format(Calendar.getInstance().getTime());
         logFile = new File ("src/main/resources/" + today + " log.txt");
         PrintWriter pw = new PrintWriter(logFile);
         if (logFile.canWrite()) {
@@ -40,5 +90,18 @@ public class FileHandling {
             logFile = new File ("src/main/resouces/" + today + " log.txt");
             pw.write(logTime + " " + log);
         }
+    }
+
+    /**
+     * Method that logs you in
+     * @throws IOException
+     */
+    public void login() throws IOException {
+        PageFactory.initElements(driver, this);
+        hold.until(ExpectedConditions.elementToBeClickable(loginButtonForm));
+        loginButtonForm.click();
+        username.sendKeys(user);
+        pass.sendKeys(password);
+        loginButtonSubmit.click();
     }
 }
--- src/main/java/com/pelian/learns/testing/SeleniumTest.java
+++ src/main/java/com/pelian/learns/testing/SeleniumTest.java
@@ -3,78 +3,58 @@ package com.pelian.learns.testing;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.*;
 
-public class SeleniumTest extends FileHandling{
-    @FindBy(xpath = "//input[@name = 'login_username']")
-    private WebElement username;
+public class SeleniumTest {
 
-    @FindBy (xpath = "//input[@name = 'login_password']")
-    private WebElement pass;
+    FileHandling fileHandling = new FileHandling();
 
-    @FindBy (xpath = "//div[@class = 'logged-in-as-wrap']/a[2]")
-    private WebElement logoutButton;
-
-    @FindBy (xpath = "//span[@class = 'a-like bold']")
-    private WebElement loginButtonForm;
-
-    @FindBy (xpath = "//input[@name = 'login']")
-    private WebElement loginButtonSubmit;
-
-    @FindBy (xpath = "//div[@class = 'logged-in-as-wrap']/a[1]")
-    private WebElement loggedUser;
-
-    @FindBy (xpath = "//input[@id = 'search-text']")
-    private WebElement searchInput;
-
-    @FindBy (xpath = "//input[@id = 'search-submit']")
-    private WebElement searchSubmit;
-    private WebDriver driver;
-    private WebDriverWait hold;
-
+    /**
+     * Method to initialize WebDriver and necessary functions for a test
+     */
     @Before
     public void initialize() {
+        // Initializing WebDriver and WebElements PageFactory
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        driver = new ChromeDriver();
-        PageFactory.initElements(driver, this);
-        hold =  new WebDriverWait(driver, 5);
+        fileHandling.driver = new ChromeDriver();
+        PageFactory.initElements(fileHandling.driver, this);
+        fileHandling.hold =  new WebDriverWait(fileHandling.driver, 5);
     }
 
-    public void login() throws IOException {
-        hold.until(ExpectedConditions.elementToBeClickable(loginButtonForm));
-        loginButtonForm.click();
-        username.sendKeys(user);
-        pass.sendKeys(password);
-        loginButtonSubmit.click();
-    }
-
+    /**
+     * Method to enter a website, log in using existing credentials, put a search inquiry and log out
+     * @throws IOException
+     */
     @Test
     public void testLoginSearch() throws IOException {
-        driver.get("https://rutracker.org");
-        logToFile("Enter a website");
-        read();
-        logToFile("Read the file with credentials");
-        login();
-        logToFile("Enter login and password");
-        searchInput.sendKeys("The infestation");
-        searchSubmit.click();
-        logToFile("Enter a query in a search field");
-        hold.until(ExpectedConditions.elementToBeClickable(logoutButton));
-        logToFile("Wait for logout button to be clickable");
-        logoutButton.click();
-        logToFile("Logout");
+        // Login
+        fileHandling.driver.get(fileHandling.website);
+        fileHandling.logToFile("Enter a website");
+        fileHandling.read();
+        fileHandling.logToFile("Read the file with credentials");
+        fileHandling.login();
+
+        // Search
+        fileHandling.logToFile("Enter login and password");
+        fileHandling.searchInput.sendKeys("The infestation");
+        fileHandling.searchSubmit.click();
+        fileHandling.logToFile("Enter a query in a search field");
+
+        // Log out
+        fileHandling.hold.until(ExpectedConditions.elementToBeClickable(fileHandling.logoutButton));
+        fileHandling.logToFile("Wait for logout button to be clickable");
+        fileHandling.logoutButton.click();
+        fileHandling.logToFile("Logout");
     }
 
     @After
     public void close() {
-        driver.quit();
+        // Close all instances of WebDriver
+        fileHandling.driver.quit();
     }
 }
